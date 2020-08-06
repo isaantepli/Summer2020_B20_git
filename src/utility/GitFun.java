b2958803a1b4edc97adbf1bf5455555829a1b4ce
isaantepli <66892614+isaantepli@users.noreply.github.com>
2020-08-05 19:32:47 -0600

    Update GitFun.java

--- src/utility/GitFun.java
+++ src/utility/GitFun.java
@@ -4,6 +4,7 @@ public class GitFun {
     public static void main(String[] args) {
         System.out.println("I can do it !");
         System.out.println("I will do it!");
+        // test comment from Github
 
     }
 }

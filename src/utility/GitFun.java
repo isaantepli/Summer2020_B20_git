fdd987e77f2bb02c3fca29d03f7e5a1e4c8e8dba
isaantepli <isaantepli46@gmail.com>
2020-08-05 19:02:22 -0600

    updated

--- src/utility/GitFun.java
+++ src/utility/GitFun.java
@@ -3,5 +3,7 @@ package utility;
 public class GitFun {
     public static void main(String[] args) {
         System.out.println("I can do it !");
+        System.out.println("I will do it!");
+
     }
 }
